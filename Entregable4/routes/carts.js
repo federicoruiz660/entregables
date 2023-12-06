100b52521fb69cfc211ab9bc168e36f386626856
federicoruiz660 <116216437+federicoruiz660@users.noreply.github.com>
2023-12-05 23:27:44 -0300

    Delete Entregable4 directory

--- Entregable4/routes/carts.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const cartsController = require('../controllers/cartsController');
-
-router.post('/', cartsController.createCart);
-router.get('/:cid', cartsController.getCartProducts);
-router.post('/:cid/product/:pid', cartsController.addProductToCart);
-
-module.exports = router;
